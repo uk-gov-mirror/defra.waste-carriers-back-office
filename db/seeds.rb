fc416c936769cc7b692d1f42a1e650372250635a
Iris Faraway <iris.faraway@gmail.com>
2018-08-09 11:28:52 +0100

    Change seed email (#71)
    We now seed at @wcr.gov.uk instead of @waste-exemplar.gov.uk.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 User.find_or_create_by(
-  email: "bo-user@waste-exemplar.gov.uk",
+  email: "bo-user@wcr.gov.uk",
   password: ENV["WCRS_DEFAULT_PASSWORD"] || "Secret123",
   confirmed_at: Time.new(2015, 1, 1)
 )
