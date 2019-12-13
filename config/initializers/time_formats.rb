7c457f69d96fc065620fcd0baa35fce3e5e7b4a4
cintamani <cintamani.puddu@gmail.com>
2019-12-13 15:37:30 +0100

    Add formatted date for order copy cards mailer (#509)
    From: https://eaflood.atlassian.net/browse/RUBY-814
    
    Add formatted string needed to the mailer class

--- config/initializers/time_formats.rb
+++ config/initializers/time_formats.rb
@@ -4,3 +4,5 @@
 Time::DATE_FORMATS[:day_month_year_slashes] = "%d/%m/%Y"
 # For example: Monday 2 December 2019 at 6:53pm
 Time::DATE_FORMATS[:weekday_day_month_year_at_time] = "%A %e %B %Y at %l:%M%P"
+# For example: 2 December 2019
+Time::DATE_FORMATS[:day_month_year] = "%d %B %Y"
