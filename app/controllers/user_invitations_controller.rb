bb62ac28f4a76fce7123f61b18d317b0e1758a7c
Iris Faraway <iris.faraway@gmail.com>
2020-01-17 09:36:48 +0000

    Fix wonky validation on user invite form (#591)
    https://eaflood.atlassian.net/browse/RUBY-760
    
    When we POST the invite form, there is an extra check to make sure that the user submitting the form has invited the new user to a group which they have permission for. (Eg a finance super has not invited an agency user).
    
    This check was a little overzealous and was causing the other validation errors to not display correctly. Now it will only fire if the user has submitted a role and the role is invalid. We still don't display a helpful error but since this will only happen if the user is tinkering with the HTML of the form or trying to submit their own custom POST requests, I am not so worried about being helpful.
    
    Also spotted that the link to email errors did not work and the error was not displayed next to the field. This PR fixes that as well.

--- app/controllers/user_invitations_controller.rb
+++ app/controllers/user_invitations_controller.rb
@@ -7,10 +7,10 @@ class UserInvitationsController < Devise::InvitationsController
   before_action :configure_permitted_parameters
 
   def create
-    if selected_role_is_in_allowed_group?(params.dig(:user, :role))
-      super
-    else
+    if selected_role_is_not_in_valid_group?
       new
+    else
+      super
     end
   end
 
@@ -27,5 +27,13 @@ class UserInvitationsController < Devise::InvitationsController
 
   def after_invite_path_for(_resource)
     users_path
+  end
+
+  def selected_role_is_not_in_valid_group?
+    role = params.dig(:user, :role)
+    return false if role.blank?
+    return false if selected_role_is_in_allowed_group?(role)
+
+    true
   end
 end
