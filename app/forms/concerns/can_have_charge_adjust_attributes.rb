61c99227d268202db203f8f9859e34200f0c9eac
cintamani <cintamani.puddu@gmail.com>
2020-02-18 12:17:57 +0000

    Validate amounts to be greater than 1 cent (#695)
    * Validate amounts to be greater than 1 cent
    
    This fixes our amounts validation to make sure we don't create payments for amounts less than 0.01

--- app/forms/concerns/can_have_charge_adjust_attributes.rb
+++ app/forms/concerns/can_have_charge_adjust_attributes.rb
@@ -6,7 +6,7 @@ module CanHaveChargeAdjustAttributes
   included do
     attr_accessor :amount, :reference, :description
 
-    validates :amount, presence: true, numericality: { greater_than: 0 }
+    validates :amount, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
     validates :reference, presence: true
     validates :description, presence: true, length: { maximum: 500 }
   end
