b28e60d004ea1d45a783b71da19f6f6e3c9284c7
cintamani <cintamani.puddu@gmail.com>
2020-05-04 15:35:19 +0100

    Update class name to match file (#842)
    From: https://eaflood.atlassian.net/browse/RUBY-250

--- app/services/second_renewal_reminder_service.rb
+++ app/services/second_renewal_reminder_service.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class FirstRenewalReminderService < RenewalReminderServiceBase
+class SecondRenewalReminderService < RenewalReminderServiceBase
   private
 
   def send_email(registration)
