ebc9af4d9d2fb20ecac92390c2b43377e13ad25b
cintamani <cintamani.puddu@gmail.com>
2020-06-12 09:13:51 +0100

    Cleanup after removing link from completed pages (#906)
    From: https://eaflood.atlassian.net/browse/RUBY-1077

--- app/helpers/waste_carriers_engine/journey_links_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module WasteCarriersEngine
-  module JourneyLinksHelper
-    def renewal_finished_link(reg_identifier:)
-      main_app.renewing_registration_path(reg_identifier: reg_identifier)
-    end
-  end
-end
