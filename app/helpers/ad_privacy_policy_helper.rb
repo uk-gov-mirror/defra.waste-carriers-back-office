198590a9e65e4862baa6556ab6560c4b8838a3a1
Iris Faraway <iris.faraway@gmail.com>
2020-07-07 09:32:21 +0100

    Update link to privacy policy (#930)
    https://eaflood.atlassian.net/browse/RUBY-1074
    
    We should link to the front office instead of the frontend.

--- app/helpers/ad_privacy_policy_helper.rb
+++ app/helpers/ad_privacy_policy_helper.rb
@@ -2,7 +2,7 @@
 
 module AdPrivacyPolicyHelper
   def link_to_privacy_policy
-    path = File.join(Rails.configuration.wcrs_frontend_url, "privacy")
+    path = File.join(Rails.configuration.wcrs_renewals_url, "/fo/pages/privacy")
 
     link_to(t(".privacy_policy"), path, target: "_blank")
   end
