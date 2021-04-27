ce40cbf31f451be0ea2bb9cdb8b2dab68c311f38
Toby Privett <toby@snaplab.co.uk>
2021-04-27 14:05:31 +0100

    Send second reminder emails from Notify (#1085)
    https://eaflood.atlassian.net/browse/RUBY-1282

--- app/services/second_renewal_reminder_service.rb
+++ app/services/second_renewal_reminder_service.rb
@@ -4,7 +4,9 @@ class SecondRenewalReminderService < RenewalReminderServiceBase
   private
 
   def send_email(registration)
-    RenewalReminderMailer.second_reminder_email(registration).deliver_now
+    Notify::RenewalReminderEmailService.run(registration: registration)
+  rescue StandardError => e
+    Airbrake.notify(e, registration_no: registration.reg_identifier) if defined?(Airbrake)
   end
 
   def expires_in_days
---
+++ spec/services/second_renewal_reminder_service_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe SecondRenewalReminderService do
+  let!(:registration) do
+    create(:registration, :active, expires_on: 28.days.from_now)
+  end
+
+  describe ".run" do
+    subject { described_class.run }
+
+    it "calls the notify renewal reminder email service" do
+      expect(Notify::RenewalReminderEmailService)
+        .to receive(:run)
+        .with(registration: registration)
+        .once
+
+      subject
+    end
+
+    context "when that service fails" do
+      it "notifies Airbrake" do
+        the_error = StandardError.new("Oopss!")
+
+        allow(Notify::RenewalReminderEmailService)
+          .to receive(:run)
+          .and_raise(the_error)
+
+        expect(Airbrake)
+          .to receive(:notify)
+          .with(the_error, { registration_no: registration.reg_identifier })
+
+        subject
+      end
+    end
+  end
+end
