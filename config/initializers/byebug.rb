b6cddb412720e31a824f9294f3770a8cd7f5bb19
David Georgiou <david.georgiou@gmail.com>
2022-07-26 13:28:11 +0100

    rubocop

--- config/initializers/byebug.rb
+++ config/initializers/byebug.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 if Rails.env.development?
-  require 'byebug/core'
+  require "byebug/core"
   begin
-    Byebug.start_server 'localhost', ENV.fetch('BYEBUG_PORT_BACK_OFFICE', 8989).to_i
+    Byebug.start_server "localhost", ENV.fetch("BYEBUG_PORT_BACK_OFFICE", 8989).to_i
   rescue Errno::EADDRINUSE
-    Rails.logger.debug 'Byebug server already running'
+    Rails.logger.debug "Byebug server already running"
   end
 end
