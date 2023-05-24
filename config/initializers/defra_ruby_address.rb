c8d443488443166b3e99f35dca59fcceff6fb052
Jerome Pratt <jerome.pratt@defra.gov.uk>
2023-05-24 10:14:59 +0100

    Add configuration host for ruby address (#1622)

--- config/initializers/defra_ruby_address.rb
+++ config/initializers/defra_ruby_address.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
 DefraRuby::Address.configure do |configuration|
-  configuration.key = ENV.fetch("ADDRESS_FACADE_CLIENT_KEY", nil)
-  configuration.client_id = ENV.fetch("ADDRESS_FACADE_CLIENT_ID", nil)
+  configuration.host = ENV.fetch("WCRS_OS_PLACES_DOMAIN", nil)
 end
