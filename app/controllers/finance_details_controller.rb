256a9f1587f316aa15575b35b9d3a7d78d555a62
brujeo <brujev@gmail.com>
2023-06-27 14:17:15 +0100

    RUBY-2528: switching from original engine template to app dedicated template when displaying payment details (#1652)

--- app/controllers/finance_details_controller.rb
+++ app/controllers/finance_details_controller.rb
@@ -6,6 +6,7 @@ class FinanceDetailsController < ApplicationController
   prepend_before_action :authenticate_user!
 
   def show
+    @resource = RegistrationPresenter.new(@resource, view_context)
     @finance_details = @resource.finance_details
   end
 end
