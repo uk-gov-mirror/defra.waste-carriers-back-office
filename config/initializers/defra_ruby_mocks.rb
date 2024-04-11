2a49204903d56fe7b7038f4802ce09ef5787afda
Jerome Pratt <jerome.pratt@defra.gov.uk>
2024-04-11 08:24:11 +0100

    [RUBY-2919] Update Defra Ruby Mocks config to point to BO GovPay mocks

--- config/initializers/defra_ruby_mocks.rb
+++ config/initializers/defra_ruby_mocks.rb
@@ -10,5 +10,5 @@ DefraRubyMocks.configure do |configuration|
 
   # Govpay API mock details. Note FO application point to BO mocks and vice-versa by defafult
   # so they don't block in a single-process application (e.g. local vagrant).
-  configuration.govpay_domain = File.join(ENV["WCRS_BO_GOVPAY_DOMAIN"] || "http://localhost:3002", "/fo/mocks/govpay/v1")
+  configuration.govpay_domain = File.join(ENV["WCRS_BO_GOVPAY_DOMAIN"] || "http://localhost:3002", "/bo/mocks/govpay/v1")
 end
