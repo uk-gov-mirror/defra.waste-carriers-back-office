600695433492f891cea549865f6b15bf18b1547f
brujeo <brujev@gmail.com>
2024-06-10 10:33:24 +0100

    Feature/ruby 2859 wcr ensure that scheduled job start and completion events are being recorded in logs (#1965)
    * [RUBY-2859] adding a task that would run rake task and format the output
    
    * [RUBY-2859] feat: Add timestamp to task start and finish messages
    
    This commit adds a timestamp to the task start and finish messages in the `rake_and_format` script. The timestamp is formatted as `YYYY-MM-DD HH:MM:SS` using the `DateTime.now.strftime` method. This change improves the readability and provides more context for the task execution.
    
    * [RUBY-2859] chore: Remove unnecessary blank line in schedule.rb file
    
    * [RUBY-2859] feat: Update schedule.rb to use rake_and_format instead of rake
    
    This commit modifies the schedule.rb file to use the rake_and_format task instead of the rake task. This change ensures that the correct tasks are executed when running the schedule.
    
    * [RUBY-2859] feat: Remove unnecessary rake tasks
    
    The commit removes the unnecessary rake tasks `one` and `two` from the `test.rake` file. These tasks were used for test purposes and no longer needed

--- config/schedule.rb
+++ config/schedule.rb
@@ -8,8 +8,13 @@
 # Learn more: http://github.com/javan/whenever
 
 log_output_path = ENV["EXPORT_SERVICE_CRON_LOG_OUTPUT_PATH"] || "/srv/ruby/waste-carriers-back-office/shared/log/"
-set :output, File.join(log_output_path, "whenever_cron.log")
+set :output, {
+  standard: File.join(log_output_path, "whenever_cron.log"),
+  error: File.join(log_output_path, "whenever_cron_error.log")
+}
 set :job_template, "/bin/bash -l -c 'eval \"$(rbenv init -)\" && :job'"
+# this job type is used to run a rake task and format standard stdout output into the log format
+job_type :rake_and_format, "cd :path && :environment_variable=:environment bin/rake_and_format :task --silent :output"
 
 # Only one of the AWS app servers has a role of "db"
 # see https://gitlab-dev.aws-int.defra.cloud/open/rails-deployment/blob/master/config/deploy.rb#L69
@@ -19,24 +24,24 @@ set :job_template, "/bin/bash -l -c 'eval \"$(rbenv init -)\" && :job'"
 # active exemptions and put this into an AWS S3 bucket from which the company
 # that provides and maintains the Electronis Public Register will grab it
 every :day, at: ENV["EXPORT_SERVICE_EPR_EXPORT_TIME"] || "21:05", roles: [:db] do
-  rake "reports:export:epr"
+  rake_and_format "reports:export:epr"
 end
 
 # This is the job to generate the daily finance reports.
 every :day, at: ENV["GENERATE_FINANCE_REPORTS_TIME"] || "03:40", roles: [:db] do
-  rake "reports:export:generate_finance_reports"
+  rake_and_format "reports:export:generate_finance_reports"
 end
 
 # This is the first renewal email reminder job. For each registration expiring
 # in 28 days time, it will generate and send the first email reminder
 every :day, at: ENV["FIRST_RENEWAL_EMAIL_REMINDER_DAILY_RUN_TIME"] || "02:05", roles: [:db] do
-  rake "email:renew_reminder:first:send"
+  rake_and_format "email:renew_reminder:first:send"
 end
 
 # This is the second renewal email reminder job. For each registration expiring
 # in 7 days time, it will generate and send the second email reminder
 every :day, at: ENV["SECOND_RENEWAL_EMAIL_REMINDER_DAILY_RUN_TIME"] || "04:05", roles: [:db] do
-  rake "email:renew_reminder:second:send"
+  rake_and_format "email:renew_reminder:second:send"
 end
 
 # This is the BOXI export job. When run this will generate a zip file of CSV's,
@@ -45,48 +50,48 @@ end
 # S3 from where it is grabbed by a process that imports it into the WCR BOXI
 # universe
 every :day, at: ENV["EXPORT_SERVICE_BOXI_EXPORT_TIME"] || "22:00", roles: [:db] do
-  rake "reports:export:boxi"
+  rake_and_format "reports:export:boxi"
 end
 
 # This is the Notify AD renewal letters job. When run it will send out Notify
 # renewal letters for all AD registrations expiring in X days' time
 every :day, at: ENV["NOTIFY_AD_RENEWAL_LETTERS_TIME"] || "02:35", roles: [:db] do
-  rake "notify:letters:ad_renewals"
+  rake_and_format "notify:letters:ad_renewals"
 end
 
 # This is the Notify digital renewal letters job. When run it will send out Notify
 # renewal letters for all digital registrations expiring in X days' time
 every :day, at: ENV["NOTIFY_DIGITAL_RENEWAL_LETTERS_TIME"] || "10:00", roles: [:db] do
-  rake "notify:notifications:digital_renewals"
+  rake_and_format "notify:notifications:digital_renewals"
 end
 
 # This is the registration exemptions expiry job which will collect all active upper tier
 # registrations that have an expiry date in the past and will set their state to `EXPIRED`s
 every :day, at: ENV["EXPIRE_REGISTRATION_EXEMPTION_RUN_TIME"] || "20:00", roles: [:db] do
-  rake "expire_registration:run"
+  rake_and_format "expire_registration:run"
 end
 
 # This is the transient registration cleanup job which will delete all records
 # that are too old
 every :day, at: ENV["CLEANUP_TRANSIENT_REGISTRATIONS_RUN_TIME"] || "00:35", roles: [:db] do
-  rake "cleanup:transient_registrations"
+  rake_and_format "cleanup:transient_registrations"
 end
 
 # This is job that removes all deletable registrations to support our data
 # retention policy
 every :day, at: ENV["REMOVE_DELETABLE_REGISTRATIONS_RUN_TIME"] || "21:00", roles: [:db] do
-  rake "remove_deletable_registrations:run"
+  rake_and_format "remove_deletable_registrations:run"
 end
 
 # This job exports copy card order details for the previous week, ending at midnight on Monday.
 every :tuesday, at: ENV["EXPORT_COPY_CARD_ORDERS_RUN_TIME"] || "02:15", roles: [:db] do
-  rake "reports:export:weekly_copy_card_orders"
+  rake_and_format "reports:export:weekly_copy_card_orders"
 end
 
 every :day, at: ENV["AREA_LOOKUP"] || "01:05", roles: [:db] do
-  rake "lookups:update:missing_area"
+  rake_and_format "lookups:update:missing_area"
 end
 
 every :day, at: ENV["CLEANUP_OLD_SESSIONS_TIME"] || "01:00", roles: [:db] do
-  rake "db:sessions:trim"
+  rake_and_format "db:sessions:trim"
 end
--- spec/schedule_spec.rb
+++ spec/schedule_spec.rb
@@ -13,83 +13,83 @@ require "open3"
 RSpec.describe "Whenever::Test::Shedule" do
   let(:schedule) { Whenever::Test::Schedule.new(file: "config/schedule.rb") }
 
-  it "makes sure 'rake' statements exist" do
-    rake_jobs = schedule.jobs[:rake]
+  it "makes sure 'rake_and_format' statements exist" do
+    rake_jobs = schedule.jobs[:rake_and_format]
     expect(rake_jobs.count).to eq(13)
   end
 
   it "picks up the EPR export run frequency and time" do
-    job_details = schedule.jobs[:rake].find { |h| h[:task] == "reports:export:epr" }
+    job_details = schedule.jobs[:rake_and_format].find { |h| h[:task] == "reports:export:epr" }
 
     expect(job_details[:every][0]).to eq(:day)
     expect(job_details[:every][1][:at]).to eq("21:05")
   end
 
   it "picks up the finance reports generation run frequency and time" do
-    job_details = schedule.jobs[:rake].find { |h| h[:task] == "reports:export:generate_finance_reports" }
+    job_details = schedule.jobs[:rake_and_format].find { |h| h[:task] == "reports:export:generate_finance_reports" }
 
     expect(job_details[:every][0]).to eq(:day)
     expect(job_details[:every][1][:at]).to eq("03:40")
   end
 
   it "picks up the first email reminder run frequency and time" do
-    job_details = schedule.jobs[:rake].find { |h| h[:task] == "email:renew_reminder:first:send" }
+    job_details = schedule.jobs[:rake_and_format].find { |h| h[:task] == "email:renew_reminder:first:send" }
 
     expect(job_details[:every][0]).to eq(:day)
     expect(job_details[:every][1][:at]).to eq("02:05")
   end
 
   it "picks up the second email reminder run frequency and time" do
-    job_details = schedule.jobs[:rake].find { |h| h[:task] == "email:renew_reminder:second:send" }
+    job_details = schedule.jobs[:rake_and_format].find { |h| h[:task] == "email:renew_reminder:second:send" }
 
     expect(job_details[:every][0]).to eq(:day)
     expect(job_details[:every][1][:at]).to eq("04:05")
   end
 
   it "picks up the Notify AD renewal letters run frequency and time" do
-    job_details = schedule.jobs[:rake].find { |h| h[:task] == "notify:letters:ad_renewals" }
+    job_details = schedule.jobs[:rake_and_format].find { |h| h[:task] == "notify:letters:ad_renewals" }
 
     expect(job_details[:every][0]).to eq(:day)
     expect(job_details[:every][1][:at]).to eq("02:35")
   end
 
   it "picks up the Notify digital renewal letters run frequency and time" do
-    job_details = schedule.jobs[:rake].find { |h| h[:task] == "notify:notifications:digital_renewals" }
+    job_details = schedule.jobs[:rake_and_format].find { |h| h[:task] == "notify:notifications:digital_renewals" }
 
     expect(job_details[:every][0]).to eq(:day)
     expect(job_details[:every][1][:at]).to eq("10:00")
   end
 
   it "picks up the BOXI export run frequency and time" do
-    job_details = schedule.jobs[:rake].find { |h| h[:task] == "reports:export:boxi" }
+    job_details = schedule.jobs[:rake_and_format].find { |h| h[:task] == "reports:export:boxi" }
 
     expect(job_details[:every][0]).to eq(:day)
     expect(job_details[:every][1][:at]).to eq("22:00")
   end
 
   it "picks up the expire registrations run frequency and time" do
-    job_details = schedule.jobs[:rake].find { |h| h[:task] == "expire_registration:run" }
+    job_details = schedule.jobs[:rake_and_format].find { |h| h[:task] == "expire_registration:run" }
 
     expect(job_details[:every][0]).to eq(:day)
     expect(job_details[:every][1][:at]).to eq("20:00")
   end
 
   it "takes the transient registration cleanup execution time from the appropriate ENV variable" do
-    job_details = schedule.jobs[:rake].find { |h| h[:task] == "cleanup:transient_registrations" }
+    job_details = schedule.jobs[:rake_and_format].find { |h| h[:task] == "cleanup:transient_registrations" }
 
     expect(job_details[:every][0]).to eq(:day)
     expect(job_details[:every][1][:at]).to eq("00:35")
   end
 
   it "takes the remove_deletable_registrations execution time from the appropriate ENV variable" do
-    job_details = schedule.jobs[:rake].find { |h| h[:task] == "remove_deletable_registrations:run" }
+    job_details = schedule.jobs[:rake_and_format].find { |h| h[:task] == "remove_deletable_registrations:run" }
 
     expect(job_details[:every][0]).to eq(:day)
     expect(job_details[:every][1][:at]).to eq("21:00")
   end
 
   it "picks up the weekly export copy card orders task run frequency and time" do
-    job_details = schedule.jobs[:rake].find { |h| h[:task] == "reports:export:weekly_copy_card_orders" }
+    job_details = schedule.jobs[:rake_and_format].find { |h| h[:task] == "reports:export:weekly_copy_card_orders" }
 
     expect(job_details[:every][0]).to eq(:tuesday)
     expect(job_details[:every][1][:at]).to eq("02:15")
