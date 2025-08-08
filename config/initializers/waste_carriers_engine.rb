11f185d7b3fc797c1bd7cf8fb17cf82641403a90
Jerome Pratt <pratt.jerome@environment-agency.gov.uk>
2025-08-08 17:07:23 +0100

    [RUBY-3896] Remove duplicate companies house settings (#2185)

--- config/initializers/waste_carriers_engine.rb
+++ config/initializers/waste_carriers_engine.rb
@@ -4,12 +4,6 @@ WasteCarriersEngine.configure do |configuration|
   # Companies house API config
   configuration.companies_house_api_key = ENV.fetch("WCRS_COMPANIES_HOUSE_API_KEY", nil)
 
-  # We only want to alter the companies house URL if mocking is enabled. Else
-  # the url is handled by the defra-ruby-validators gem from the wcr engine
-  if ENV["WCRS_MOCK_ENABLED"].to_s.downcase == "true"
-    configuration.companies_house_host = ENV.fetch("WCRS_MOCK_FO_COMPANIES_HOUSE_URL", nil)
-  end
-
   # Link to dashboard from user journeys
   configuration.link_from_journeys_to_dashboards = true
 
